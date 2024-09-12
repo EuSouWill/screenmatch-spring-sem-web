e2eadafc8b0a8f6d27a6d52d021be9f8fa602150
Eusouwill <williamnunesregis@icloud.com>
2024-09-12 19:16:49 -0300

    Incio do projeto spring

---
+++ src/main/java/br/com/will/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.will.screenmatch;
+
+import br.com.will.screenmatch.model.DadosSerie;
+import br.com.will.screenmatch.service.ConsumoApi;
+import br.com.will.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=lost&apikey=313b157e");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/main/java/br/com/will/screenmatch/service/IConverteDados.java
@@ -0,0 +1,5 @@
+package br.com.will.screenmatch.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json,Class<T> classe);
+}
